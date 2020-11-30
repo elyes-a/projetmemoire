1ced44b3a146a2c49edb0c24fc023f79fb1d28f4
elyes-a <atigelyes@gmail.com>
2020-11-30 01:40:57 +0100

    remplir news and index par
    un contenu (qlq article en relation avec l'education )

--- news.php
+++ news.php
@@ -14,41 +14,30 @@ require_once 'modele_page_cour/head2.php' ;
 ">
   <div id="myCarousel" class="carousel slide" data-interval="false" data-ride="carousel" data-pause="hover" style="margin-bottom: 2%;">
     <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class=""></li>
-      <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+      <li data-target="#myCarousel" data-slide-to="0" class="" style="background-color: blue;"></li>
+      <li data-target="#myCarousel" data-slide-to="1" class="active" style="background-color: blue;" ></li>
+      <li data-target="#myCarousel" data-slide-to="2" class="" style="background-color: blue;"></li>
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item">
-        <!--la balise <svg> est utile pour appeler le SVG dans un document HTML. Le SVG est un format d’image XML, qui vous donne la possibilité de créer des formes simples et complexes à intégrer dans votre document HTML sans recourir à des logiciels PAO.-->
-        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="red"></rect></svg>
+        <img src="img/inscription_6eme2021.png" class="img-fluid">
         <div class="container">
-          <div class="carousel-caption text-left">
-            <h1>Example headline.</h1>
-            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+          <div class="carousel-caption text-left" style=" top: 5%; left: 4%;">
+            <p><a class="btn btn-lg btn-primary" href="http://www.six.edunet.tn/" target="_blank" role="button">voir le site</a></p>
           </div>
         </div>
       </div>
       <div class="carousel-item active">
-        <img src="css/electricity-experiment-picture.jpg" class="img-fluid">
-        
-          <div class="carousel-caption">
-            <h1>Another example headline.</h1>
-            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-          </div>
-        
-      </div>
-      <div class="carousel-item">
-        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"></rect></svg>
-        <div class="container">
-          <div class="carousel-caption text-right">
-            <h1>One more for good measure.</h1>
-            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
-          </div>
+        <img src="img/inscription_9eme2021.png" class="img-fluid">
+          <div class="carousel-caption text-left" >
+            <p><a class="btn btn-lg btn-primary" href="http://www.neuf.edunet.tn/" target="_blank" role="button">voir le site</a></p>
         </div>
+      </div>
+      <div class="carousel-item ">
+        <img src="img/inscri_bac2021.png" class="img-fluid">
+          <div class="carousel-caption" style=" right: 32%; left: 7%; bottom: 4%">
+            <p><a class="btn btn-lg btn-primary" href="http://www.inscription.edunet.tn/" target="_blank" role="button"> voir le site</a></p>
+          </div>
       </div>
     </div>
     <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
@@ -75,25 +64,39 @@ require_once 'modele_page_cour/head2.php' ;
 
     <!-- Three columns of text below the carousel -->
     <div class="row">
-      <div class="col-lg-4">
-        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="yellow"></rect><text x="50%" y="50%" fill="red" dy=".3em">140x140</text></svg>
-        <h2>Heading</h2>
-        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
-      </div><!-- /.col-lg-4 -->
-      <div class="col-lg-4">
-        <img class="bd-placeholder-img rounded-circle" width="140" height="140" preserveaspectratio="xMidYMid slice" focusable="false" role="img" src="css/electricity-experiment-picture.jpg" alt=" Your text">
-        <figcaption></figcaption>
-        <h2>Heading</h2>
-        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
-      </div><!-- /.col-lg-4 -->
-      <div class="col-lg-4">
-        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="true" role="img" aria-label="Placeholder: 140x140"><!-- titre3 vu lors l'image est hover--><title>title3</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="green" dy=".3em">140x140</text></svg>
-        <h2>Heading</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
-      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-8">
+        <a href="https://www.unicef.org/tunisia/"><img class="bd-placeholder-img " width="100%" height="auto" preserveaspectratio="xMidYMid slice" focusable="false" role="img" src="img/enfant.png" alt="&copy; unicef tunisia"><figcaption></figcaption></a>
+        <h2> la Journée mondiale de l’enfance</h2>
+        <p>La Tunisie célèbre la Journée mondiale de l’enfance et le 31ème anniversaire de la Convention relative aux droits de l’enfant et annonce à cette occasion la création d’un Conseil supérieur de l’enfance</p>
+        <p><a class="btn btn-secondary" href="https://www.unicef.org/tunisia/recits/la-tunisie-c%C3%A9l%C3%A8bre-la-journ%C3%A9e-mondiale-de-lenfance" role="button">voir détails </a></p>
+      </div><!-- /.col-lg-8 -->
+      <div class="col-lg-4" style="
+    padding-left: 15px;
+    padding-right: 15px;
+    float: left;
+">
+        <aside id="linkcat-2" class="widget widget_links" style="
+    width: 100%;
+    margin-bottom: 30px;
+"><h2 class="widget-title" style="
+    background: #294a70 none repeat scroll 0 0;
+    border-bottom: 0 solid #ffab1f;
+    border-left: 20px solid #ffab1f;
+    color: #ffffff;
+    font-size: 16px;
+    font-weight: normal;
+    line-height: 1.5;
+    padding: 5px 15px;
+    position: relative;
+    margin-bottom: 20px;
+">Liens utiles</h2>
+          <ul class="xoxo blogroll" style="">
+            <li style="font-family: fontawesome;font-size: 2rem;margin-left: 5px;margin-right: 8px;"><a href="http://www.6web.edunet.tn/" target="_blank">Concours 6ème</a></li>
+            <li style="font-family: fontawesome;font-size: 2rem;margin-left: 5px;margin-right: 8px;"><a href="http://www.9web.edunet.tn/" target="_blank">Concours 9ème</a></li>
+            <li style="font-family: fontawesome;font-size: 2rem;margin-left: 5px;margin-right: 8px;"><a href="http://www.bacweb.tn/" target="_blank">Concours Bac</a></li>
+          </ul>
+        </aside>
+      </div><!-- /.col-lg-4-->
     </div><!-- /.row -->
 
 
@@ -103,23 +106,13 @@ require_once 'modele_page_cour/head2.php' ;
 
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        <h2 class="featurette-heading">Suivis état sanitaire  <hr><span class="text-muted">le 24 novembre</span></h2>
+        <p class="lead"></p>
       </div>
       <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="red"></rect><text x="50%" y="50%" fill="yallow" dy=".3em"></text></svg>
-      </div>
-    </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-      <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-      </div>
-      <div class="col-md-5 order-md-1">
-        <img src="css/electricity-experiment-picture.jpg" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" preserveaspectratio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><figcaption>500*500</figcaption>
+        <a href="img/Suiv_EtatSanit20Nov.png" target="_blank">
+          <img src="img/Suiv_EtatSanit24Nov.png" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" preserveaspectratio="xMidYMid slice" focusable="false" role="img" aria-label="Suivis état sanitaire 20 nov">
+        </a>
       </div>
     </div>
     <!-- /END THE FEATURETTES -->
